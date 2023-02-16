c03ab5d057c2c2268689f23552266239f2f43e42
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-16 11:38:03 -0800

    Added 4-puts.c File

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * a C program that prints exactly a line with puts func.
+ **/
+
+int main (void) {
+    puts("\"Programming is like building a multilingual puzzle");
+    
+    return 0;
+}
