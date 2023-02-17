7a504e1e7fec351f0d9df95efd787dcf9d303cb9
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-17 14:04:55 -0800

    Added 3-print_alphabets.c File

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a C program to print the alphabet in lowercase and then in uppercase.
+ * Return: 0 (For Success)
+ **/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+                putchar(alphabet);
+
+	putchar('\n');
+	return (0);
+}
