ecb649fdec042c5183bc922605792494ad3a46cc
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-17 13:53:42 -0800

    Added 2-print_alphabet.c File

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main - a C program to print the last digit of the number.
+ * main - a C program to print the alphabet in lowercase.
  * Return: 0 (For Success)
  **/
 int main(void)
 {
-	int n;
-	int m;
+	char y = 'y';
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	m = n % 10;
+	while (y < 'z')
+	{
+		putchar(y);
+		y++;
 
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, m);
-	else if (m == 0)
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	else if (m < 6 && m != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+	}
+
+	putchar('\n');
 	return (0);
 }
