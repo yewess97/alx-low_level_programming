5d300f00a57b607d76572ae41f81baa5b0976de6
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-17 14:12:36 -0800

    Added 4-print_alphabt.c File

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a C program to print the alphabet in lowercase.
+ * Return: 0 (For Success)
+ **/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q' && alphabet == 'e')
+			continue;
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+	return (0);
+}
