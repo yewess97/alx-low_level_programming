69d63cd562577e2952e6b4b6c7872a4e0d786563
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-17 15:07:02 -0800

    Added 102-print_comb5.c File

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int main(void)
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
 			putchar(' ');
-			putchar((num1 % 10) + '0');
+			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
 			if (num1 == 98 && num2 == 99)
