a2a467d50a5b0033f0061227b0eb40650e7f0857
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 13:06:27 -0800

    Added 2-print_alphabet_x10.c File

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - a C program that prints 10 times the alphabet in lowercase.
+ * print_alphabet_x10 - a C program that prints 10x the alphabet in lowercase.
  **/
 void print_alphabet_x10(void)
 {
