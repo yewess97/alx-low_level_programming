62d49838ca8dcc5a1294e8807d82c9a17bb9f885
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 14:54:52 -0800

    Added 100-times_table.c File

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * print_times_table - a C program that prints the n times table, starting with 0.
+ * @n: the number of times that table to be printed
+ **/
+void print_times_table(int n)
+{
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod < 10 || prod < 100)
+				{
+					_putchar(' ');
+				}
+				else if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+				}
+				else if (prod >= 10 && prod < 100)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
