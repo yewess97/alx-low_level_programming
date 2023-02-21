01741275aa0d0d2372c3b7a7840283fe073c1466
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 13:34:24 -0800

    Added 6-abs.c File

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - a C program that computes the absolute value of an integer.
+ * @a: the integer to be computed
+ * Return: a (if positive), -a (if negative)
+ **/
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-a);
+	}
+}
