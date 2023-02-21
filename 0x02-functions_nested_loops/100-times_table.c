77091c6e998383dff55072ff3aa5a34688a58520
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 15:29:02 -0800

    Added 100-times_table.c File

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,34 +8,41 @@ void print_times_table(int n)
 {
 	int num, mult, prod;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
 	{
-		for (num = 0; num <= n; num++)
+		return;
+	}
+	for (num = 0; num <= n; num++)
+	{
+		for (mult = 0; mult <= n; mult++)
 		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			prod = num * mult;
+
+			if (mult != n)
 			{
 				_putchar(',');
 				_putchar(' ');
-
-				prod = num * mult;
-
-				if (prod < 10 || prod < 100)
-				{
-					_putchar(' ');
-				}
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar((prod / 10) % 10 + '0');
-				}
-				else if (prod >= 10 && prod < 100)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
 			}
-			_putchar('\n');
+			if (mult != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			_putchar(prod + '0');
+			if (prod > 99)
+			{
+				_putchar(prod / 100 + '0');
+				_putchar((prod / 10 % 10) + '0');
+			}
+			else if (prod > 9)
+			{
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+			}
+			_putchar(prod % 10 + '0');
 		}
+		_putchar('\n');
 	}
+
 }
