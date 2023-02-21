b9158e2f32ef939a4f7b5b98af8d8e1a2ead97c2
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 13:44:56 -0800

    Added 7-print_last_digit.c File

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,12 @@ int print_last_digit(int num)
 	int ld = num % 10;
 
 	if (ld < 0)
+	{
 		ld *= -1;
-	_putchar(ld + '0');
+		_putchar(ld + '0');
+		return (ld);
+	}
 
-	return (0);
+	_putchar(ld + '0');
+	return (ld);
 }
