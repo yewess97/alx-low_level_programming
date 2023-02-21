0f5cd2581d9b0fd963f1b482d62abea902cd3ce1
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 13:11:30 -0800

    Added 3-islower.c File

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - a C program that checks for lowercase character.
+ * @c: the character
+ * Return: 1 (For Success), 0 (For Failure)
+ **/
+void _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
