1b4b78e0914d7508d08b3159ebb6d0317ac4dcf3
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 12:42:26 -0800

    Added 1-alphabet.c File

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - a C program that prints the alphabet, in lowercase.
+ * Return: 0 (For Success)
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
