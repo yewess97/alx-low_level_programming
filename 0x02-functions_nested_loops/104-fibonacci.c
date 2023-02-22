eaea325d8488d8510a35739d46bae54c7ec972ef
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 18:00:41 -0800

    Added 104-fibonacci.c File

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define LARGEST 10000000000
+/**
+ * main - a C program that prints the first 98 Fibonacci numbers.
+ * description: starting with 1 and 2.
+ * Return: 0 (For Success)
+ **/
+int main(void)
+{
+	unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
+	unsigned long int hold1, hold2, hold3;
+	int count;
+
+	printf("%lu, %lu, ", bk1, bk2);
+	for (count = 2; count < 98; count++)
+	{
+		if (count != 97)
+                        printf(", ");
+		if (bk1 + bk2 > LARGEST || fr2 > 0 || fr1 > 0)
+		{
+			hold1 = (bk1 + bk2) / LARGEST;
+			hold2 = (bk1 + bk2) % LARGEST;
+			hold3 = fr1 + fr2 + hold1;
+			fr1 = fr2, fr2 = hold3;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu%010lu", fr2, bk2);
+		}
+		else
+		{
+			hold2 = bk1 + bk2;
+			bk1 = bk2, bk2 = hold2;
+			printf("%lu", bk2);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
