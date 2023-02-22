c199d957d69b89a515a69a9e057bd0923fc057be
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 17:38:26 -0800

    Added 102-fibonacci.c File

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints the first 50 Fibonacci numbers, starting with 1 and 2.
+ * Return: 0 (For Success)
+ **/
+int main(void)
+{
+	int i;
+	long j = 1, k = 2;
+
+	for (i = 0; i < 50; i++)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf("%ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
