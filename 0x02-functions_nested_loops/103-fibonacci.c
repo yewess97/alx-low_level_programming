a9335a84af373a8e28696de65d65635e85002173
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-21 17:50:36 -0800

    Added 103-fibonacci.c File

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints the sum of the even-valued terms.
+ * description: not exceeding 4,000,000.
+ * Return: 0 (For Success)
+ **/
+int main(void)
+{
+	int i;
+	long j = 1, k = 2, sum = k;
+
+	for (i = 0; k + j < 4000000; i++)
+	{
+		k += j;
+		if (k % 2 == 0)
+			sum += k;
+		j = k - j;
+	}
+	printf("%ld", sum);
+
+	return (0);
+}
