b2c8052ca48197bb56b55abc5f604d8e737c2bc1
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-24 14:27:57 -0800

    Added 6-print_line.c File

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,7 +6,7 @@
 void print_line(int n)
 {
 	int i;
-	
+
 	if (n <= 0)
 	{
 		_putchar('\n');
@@ -15,7 +15,7 @@ void print_line(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(95);
+			_putchar('_');
 		}
 		_putchar('\n');
 	}
