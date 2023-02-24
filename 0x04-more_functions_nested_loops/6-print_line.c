9e8814cece1f395dfd481daa9f756c875c06466a
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-24 13:23:07 -0800

    Added 6-print_line.c File

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - a C program that draws a straight line in the terminal.
+ * @n: The number of times the character _ should be printed
+ **/
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_puthchar('_');
+		}
+		_putchar('\n');
+	}
+}
