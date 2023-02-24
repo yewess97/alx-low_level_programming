de9e55f8eed087b64dcef7c5e95647d3d1ecf02f
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-24 15:39:36 -0800

    Added 101-print_number.c File

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,13 +5,16 @@
  **/
 void print_number(int n)
 {
+	unsigned int x = n;
+
 	if (n < 0)
 	{
 		n *= -1;
+		x = n;
 		_putchar('-');
 	}
-	n /= 10;
-	if (n != 0)
-		print_number(n);
+	x /= 10;
+	if (x != 0)
+		print_number(x);
 	_putchar((unsigned int) n % 10 + '0');
 }
