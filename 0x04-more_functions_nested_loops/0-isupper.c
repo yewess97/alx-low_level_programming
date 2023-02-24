ee8d1bc2d330d672032ae871539ccd7b4d69e13f
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-24 12:19:34 -0800

    Added 0-isupper.c File

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,14 @@
-:x
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - a C program that checks for uppercase character.
+ * @c: The letter to be checked
+ * Return: 1 (if upper), 0 (otherwise)
+ **/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
