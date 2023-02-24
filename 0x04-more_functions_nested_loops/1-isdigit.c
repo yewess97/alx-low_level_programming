ec0970a0379410bd2929c09c022a6649ced4496c
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-02-24 12:26:16 -0800

    Added 1-isdigit.c File

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isdigit - a C program that checks for a digit (0 through 9).
+ * @c: The number to be checked
+ * Return: 1 (if digit), 0 (otherwise)
+ **/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
