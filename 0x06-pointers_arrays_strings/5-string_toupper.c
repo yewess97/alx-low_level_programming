458c3097327d158d7747d880008772cf8d148afa
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-01 15:43:07 -0800

    Added 5-string_toupper.c File

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - a C program that changes all lowercase letters
+ * of a string to uppercase.
+ * @str: The string to be changed
+ * Return: A pointer to the changed string
+ **/
+char *string_toupper(char *str)
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
