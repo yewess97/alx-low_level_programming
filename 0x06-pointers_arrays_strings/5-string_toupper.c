967b3b35da697c323773c7e48a552c1f02957b29
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-01 15:46:13 -0800

    Added 5-string_toupper.c File

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,10 @@ char *string_toupper(char *str)
 {
 	int i = 0;
 
-	for (; str[i] >= 'a' && str[i] <= 'z'; i++)
+	while (str[i] >= 'a' && str[i] <= 'z')
 	{
 		str[i] -= 32;
+		i++;
 	}
 
 	return (str);
