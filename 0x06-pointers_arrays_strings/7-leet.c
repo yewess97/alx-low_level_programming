7bba89274da1bad244f76f094f63d9434425a0c3
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-01 16:34:28 -0800

    Added 7-leet.c File

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,12 +6,12 @@
  **/
 char *leet(char *n)
 {
-	int i = 0, j = 0;
+	int i, j;
 	char s1[] = "aAeEoOtTlL", s2[] = "4433007711";
 
-	for (; n[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (n[i] == s1[j])
 				n[i] = s2[j]
