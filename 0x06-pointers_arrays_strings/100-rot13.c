ee1860dbe8a1381da20f604e63cabd359773759d
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-01 17:28:58 -0800

    Added 100-rot13.c File

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - a C program that encodes a string using rot13.
+ * @str: The string to be encoded
+ * Return: A pointer to the string
+ **/
+char *rot13(char *str)
+{
+	int i, j;
+	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	
+	return (str);
+}
