638093e61d89fb3f2576b056a5439670c8bb7dac
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-06 08:32:11 -0800

    Added 2-strchr.c File

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strchr - a C program that locates a character in a string.
+ * @s: The input string
+ * @c: The input character
+ * Return: A pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found
+ **/
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return (NULL);
+}
