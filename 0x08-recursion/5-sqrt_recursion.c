622e70be537837a30dbd31b544437eeb6cd9de6c
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-08 07:12:15 -0800

    Added 5-sqrt_recursion.c File

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt - a C program that calculates the natural square root
+ * @n: A given number to calculate its square root
+ * @i: A given iterate number
+ * Return: The natural square root for (n)
+ **/
+int _sqrt(int n, int i)
+{
+	int iteration = i * i;
+
+	if (iteration > n)
+		return (-1);
+	else if (iteration == n)
+		return (i);
+	else
+		return (_sqrt(n, i++));
+}
+
+/**
+ * _sqrt_recursion - a C program that returns the natural square root
+ * of a number.
+ * * @n: A given number to calculate its natural square root
+ * Return: The natural square root of (n)
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
