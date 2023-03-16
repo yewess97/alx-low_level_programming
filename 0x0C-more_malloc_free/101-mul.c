a7e0b7db811801c503f80aa748e0dfc4e48be03a
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-16 16:52:55 -0700

    Added 101-mul.c File

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,10 +3,11 @@
 #include "main.h"
 
 /**
- * is_digit - Checks if a string contains a non-digit char
- * @s: A given string to be evaluated
- * Return: 0 (if a non-digit is found), 1 (otherwise)
- **/
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
 int is_digit(char *s)
 {
 	int i = 0;
@@ -22,9 +23,10 @@ int is_digit(char *s)
 
 /**
  * _strlen - returns the length of a string
- * @s: A given string to be evaluated
+ * @s: string to evaluate
+ *
  * Return: the length of the string
- **/
+ */
 int _strlen(char *s)
 {
 	int i = 0;
@@ -37,8 +39,8 @@ int _strlen(char *s)
 }
 
 /**
- * errors - handles errors for main method
- **/
+ * errors - handles errors for main
+ */
 void errors(void)
 {
 	printf("Error\n");
@@ -46,11 +48,12 @@ void errors(void)
 }
 
 /**
- * main - A C program that multiples two positive numbers.
- * @argc: The number of arguments
- * @argv: The arrays of arguments
- * Return: 0 (For Success)
- **/
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
@@ -65,7 +68,7 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (i = 0; i <= len1 + len2, i++;)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
