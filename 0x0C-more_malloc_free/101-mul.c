44ced602bacdd7bc71a7958de9e30695dff6d887
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-16 16:55:46 -0700

    Added 101-mul.c File

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,11 +3,10 @@
 #include "main.h"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- *
- * Return: 0 if a non-digit is found, 1 otherwise
- */
+ * is_digit - Checks if a string contains a non-digit char
+ * @s: A given string to be evaluated
+ * Return: 0 (if a non-digit is found), 1 (otherwise)
+ **/
 int is_digit(char *s)
 {
 	int i = 0;
@@ -23,10 +22,9 @@ int is_digit(char *s)
 
 /**
  * _strlen - returns the length of a string
- * @s: string to evaluate
- *
+ * @s: A given string to be evaluated
  * Return: the length of the string
- */
+ **/
 int _strlen(char *s)
 {
 	int i = 0;
@@ -39,8 +37,8 @@ int _strlen(char *s)
 }
 
 /**
- * errors - handles errors for main
- */
+ * errors - handles errors for main method
+ **/
 void errors(void)
 {
 	printf("Error\n");
@@ -48,12 +46,11 @@ void errors(void)
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: always 0 (Success)
- */
+ * main - A C program that multiples two positive numbers.
+ * @argc: The number of arguments
+ * @argv: The arrays of arguments
+ * Return: 0 (For Success)
+ **/
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
