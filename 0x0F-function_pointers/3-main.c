c5d6410d9afd1621b73eefec114c5d11427a2fd5
Yousif Ayman Ahmed <yewess97@gmail.com>
2023-03-22 16:54:59 -0700

    Added 3-main.c File

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,14 +9,18 @@
  **/
 int main(int argc, char *argv[])
 {
+	int num1, num2;
+	char *op;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	int num1 = atoi(argv[1]), num2 = atoi(argv[3]);
-        char *op = argv[2];
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
 
 	if (op[1] || !get_op_func(op))
 	{
